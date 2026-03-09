56f80c912878bd0305a7326d94ee2320344affd9
ongyongyue <yongyue555@gmail.com>
2026-03-09 21:55:06 +0800

    Edit Property Class
    Added more attirbuted and details into Property Class, changed the
    Property.toString() method as well

--- src/main/java/seedu/address/model/Property.java
+++ src/main/java/seedu/address/model/Property.java
@@ -7,18 +7,28 @@ import static java.util.Objects.requireNonNull;
  */
 public class Property {
 
+    private final String owner;
     private final String address;
     private final String type;
     private final String price;
+    private final String size;
 
-    public Property(String address, String type, String price) {
+    public Property(String owner, String address, String type, String price, String size) {
+        requireNonNull(owner);
         requireNonNull(address);
         requireNonNull(type);
         requireNonNull(price);
+        requireNonNull(size);
 
+        this.owner = owner;
         this.address = address;
         this.type = type;
         this.price = price;
+        this.size = size;
+    }
+
+    public String getOwner() {
+        return owner;
     }
 
     public String getAddress() {
@@ -35,6 +45,18 @@ public class Property {
 
     @Override
     public String toString() {
-        return address + " | " + type + " | $" + price;
+        return String.format(
+                "Owner: %s\n" +
+                "Address: %s\n" +
+                "Type: %s\n"
+                "Price : S$%s\n" +
+                "Size : %s\n",
+                this.getOwner(),
+                this.getAddress(),
+                this.getType(),
+                this.getPrice(),
+                this.getSize()
+        );
+
     }
 }
